42c1e54fc47bdf591b50cb4807667379b3ccd916
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-03-19 14:36:47 +0900

    Add custom Prelude.

---
+++ src/ASPico/Prelude.hs
@@ -0,0 +1,42 @@
+module ASPico.Prelude
+  ( module ClassyPrelude
+  , module Control.Arrow
+  , module Control.Exception
+  , module Control.Monad.Base
+  , module Control.Monad.Except
+  , module Control.Monad.Logger
+  , module Control.Monad.Reader
+  , module Control.Monad.Trans.Control
+  , module Data.Data
+  , module Data.Proxy
+  , module Data.Time.Clock
+  , module Data.Time.LocalTime
+  , module Data.Typeable
+  , module Data.Word
+  , module Data.Void
+  , module ASPico.Utils
+  , module Text.Email.Validate
+  ) where
+
+import ClassyPrelude hiding ((<.>))
+
+import Control.Arrow ((<<<), (>>>))
+import Control.Exception (SomeException(..))
+import Control.Monad.Base (MonadBase, liftBase)
+import Control.Monad.Except
+       (MonadError(..), ExceptT(..), runExceptT)
+import Control.Monad.Logger (MonadLogger, LoggingT, logDebug)
+import Control.Monad.Reader (MonadReader(reader))
+import Control.Monad.Trans.Control (MonadBaseControl)
+import Data.Data (Data)
+import Data.Proxy (Proxy(..))
+import Data.Time.Clock (NominalDiffTime)
+import Data.Time.LocalTime (ZonedTime)
+import Data.Typeable (Typeable, typeOf, typeRep)
+import Data.Word (Word16)
+import Data.Void (Void)
+import "emailaddress" Text.Email.Validate
+       (EmailAddress, emailAddress)
+
+import ASPico.Orphans ()
+import ASPico.Utils
