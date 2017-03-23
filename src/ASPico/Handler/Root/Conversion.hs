03cb66052d571fd30d42f5b8f3b60b2fdc337739
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-03-23 22:15:31 +0900

    Change to check actual cookie value.

--- src/ASPico/Handler/Root/Conversion.hs
+++ src/ASPico/Handler/Root/Conversion.hs
@@ -5,21 +5,33 @@ module ASPico.Handler.Root.Conversion
 
 import ASPico.Prelude
 
+import Database.Persist.Sql (Key(..), toSqlKey)
 import Servant ((:>), Get, Header(..), QueryParam, ServerT)
-import Web.HttpApiData (FromHttpApiData, ToHttpApiData)
+import Web.Cookie (parseCookies)
+import Web.HttpApiData (FromHttpApiData(..), ToHttpApiData)
 
-import ASPico.Db (AffiliateId, CvId)
+import ASPico.Db (Affiliate, CvId)
 import ASPico.Error (AppErr)
+import ASPico.Handler.Consts (affiliateCookie)
 import ASPico.Monad (MonadASPicoDb, dbCreateConversion)
 import ASPico.Servant (Png)
 
 type ApiConversion = "cv"
   :> QueryParam "cid" CvId
-  :> Header "Cookie" Cookie
+  :> Header "Cookie" AffiliateCookie
   :> Get '[Png] ByteString
 
-newtype Cookie = Cookie AffiliateId
-  deriving (Eq, Show, FromHttpApiData, ToHttpApiData)
+newtype AffiliateCookie = AffiliateCookie (Key Affiliate)
+  deriving (Eq, Read, Show, ToHttpApiData)
+
+instance FromHttpApiData AffiliateCookie where
+  parseUrlPiece txt =
+    case readMay . decodeUtf8 =<< maybeVal of
+      Nothing -> Left "Could not found affiliate id in cookie."
+      Just aff -> Right . AffiliateCookie . toSqlKey $ aff
+    where
+      maybeVal :: Maybe ByteString
+      maybeVal = lookup affiliateCookie . parseCookies . encodeUtf8 $ txt
 
 serverConversion
   :: (MonadError AppErr m, MonadASPicoDb m)
@@ -30,11 +42,11 @@ serverConversion =
 conversion
   :: (MonadError AppErr m, MonadASPicoDb m)
   => Maybe CvId
-  -> Maybe Cookie
+  -> Maybe AffiliateCookie
   -> m ByteString
 conversion mconvId mcookie = do
   case (mconvId, mcookie) of
-    (Just convId, Just (Cookie affId)) ->
+    (Just convId, Just (AffiliateCookie affId)) ->
       void $ dbCreateConversion
         affId
         convId
